a69a551fbc406b78f337217fc520bc37b4344676
Kuntay KOCA <kuntay.koca.development@gmail.com>
2021-12-29 02:32:10 +0300

    level1 okey

---
+++ Assets/Scripts/XleriKapatma.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XleriKapatma : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="Player")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+
+
+}
